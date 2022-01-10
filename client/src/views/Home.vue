cba204ae855235aa245127dfa6dc2f91b2ada445
刘天青 <liutq@hollycrm.com>
2022-01-10 18:24:52 +0800

    feat: 客户端建立socket连接

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="home">
-    <input type="text" name="message" />
-    <button>send</button>
+    <ul id="messages"></ul>
+
+    <form id="form" action="">
+      <input id="input" autocomplete="off" /><button>Send</button>
+    </form>
   </div>
 </template>
 
 <script>
+import { io } from 'socket.io-client';
 
 export default {
   name: 'Home',
+  mounted() {
+    // console.log(io);
+    this.init();
+  },
+  methods: {
+    init() {
+      const socket = io('http://localhost:3000/ws');
+      console.log('socket', socket);
+
+      socket.on('chat message', function(msg) {
+        console.log('msg', msg);
+        // var item = document.createElement('li');
+        // item.textContent = msg;
+        // messages.appendChild(item);
+        // window.scrollTo(0, document.body.scrollHeight);
+      });
+    },
+  },
 }
 </script>

11b26848f3be9c52706c73abf06e8d3130b7eaab
刘天青 <liutq@hollycrm.com>
2022-01-10 18:23:42 +0800

    chore: configure webpack

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,13 +2,13 @@ module.exports = {
   devServer: {
     port: 7071,
     disableHostCheck: true,
-    proxy: {
-      '/ws': {
-        target: 'http://127.0.0.1:3000',
-        changeOrigin: true,
-        ws: true,
-        pathRewrite: { '^/ws': '' }
-      },
-    }
+    // proxy: {
+    //   '/ws': {
+    //     target: 'http://127.0.0.1:3000',
+    //     changeOrigin: true,
+    //     ws: true,
+    //     pathRewrite: { '^/ws': '' }
+    //   },
+    // }
   }
 }
